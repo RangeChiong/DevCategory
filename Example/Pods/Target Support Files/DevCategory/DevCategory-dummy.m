e36dce2a355ab6f1fb45f7177d2d9a6c26c114bf
常润泽 <changrz1@centaline.com.cn>
2017-09-05 13:42:04 +0800

    First Blood
    Signed-off-by: 常润泽 <changrz1@centaline.com.cn>

---
+++ Example/Pods/Target Support Files/DevCategory/DevCategory-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DevCategory : NSObject
+@end
+@implementation PodsDummy_DevCategory
+@end
---
+++ Example/Pods/Target Support Files/Pods-DevCategory_Tests/Pods-DevCategory_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DevCategory_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_DevCategory_Tests
+@end
